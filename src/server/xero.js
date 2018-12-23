1a16f3df2f58318f8d04508446b85c6f6296ede9
Reginaldo Santos <rmsantos@br.ibm.com>
2018-12-23 16:33:26 -0200

    experimenting new ways on backend

--- src/server/xero.js
+++ src/server/xero.js
@@ -1,31 +1,45 @@
-const express = require('express')
-const router = express.Router()
-const XeroClient = require('xero-node').AccountingAPIClient
-const config = require('./../../config/config.json')
+const express = require("express");
+const router = express.Router();
+const XeroClient = require("xero-node").AccountingAPIClient;
+const axios = require("axios")
 
-let xero = new XeroClient(config)
+const config = require("./../../config/config.json");
 
-let lastRequestToken = null
+let xero = new XeroClient(config);
 
-router.get('/callback', async (req, res) => {
-  let oauth_verifier = req.query.oauth_verifier
-  let accessToken = await xero.oauth1Client.swapRequestTokenforAccessToken(lastRequestToken, oauth_verifier)
+let lastRequestToken = null;
+let response = {}
 
-  let org = await xero.organisations.get()
-  let accts = await xero.accounts.get()
-  let contacts = await xero.contacts.get()
+router.get("/access", async (req, res) => {
+  let oauth_verifier = req.query.oauth_verifier;
+  let accessToken = await xero.oauth1Client.swapRequestTokenforAccessToken(
+    lastRequestToken,
+    oauth_verifier
+  );
 
-  let response = {
-    org, accts, contacts
-  }
+  let org = await xero.organisations.get();
+  let accts = await xero.accounts.get();
+  let contacts = await xero.contacts.get();
 
+  response = {
+    org,
+    accts,
+    contacts
+  };
+  
+  res.send(response);
+});
+
+router.get("/connect", async (req, res) => {
+  lastRequestToken = await xero.oauth1Client.getRequestToken();
+  let authoriseURL = xero.oauth1Client.buildAuthoriseUrl(lastRequestToken);
+  console.log("lastRequestToken", lastRequestToken)
+  console.log("authoriseURL", authoriseURL)
+  res.redirect(authoriseURL);
+});
+
+router.get("/teste", (req, res) => {
   res.send(response)
-})
-
-router.get('/connect', async (req, res) => {
-  lastRequestToken = await xero.oauth1Client.getRequestToken()
-  let authoriseURL = xero.oauth1Client.buildAuthoriseUrl(lastRequestToken)
-  res.redirect(authoriseURL)
 })
 
 module.exports = router;
