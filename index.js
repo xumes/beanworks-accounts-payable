744d32c0ec988fcc995afc929fd988752021de7f
Reginaldo Santos <rmsantos@br.ibm.com>
2018-12-22 22:30:20 -0200

    drafting the html file (for calling frontend)

--- index.js
+++ index.js
@@ -11,15 +11,15 @@ const port = process.env.PORT || 3000
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 app.use(cookieParser());
-//app.use(session(require('./config/session')));
+app.use(session(require('./config/session')));
 
 app.use((req, res, next) => {
   return next();
 });
 
-app.get("/", (req, res) => {
-    res.send("working")
-})
+//app.use('/api', require('./src/server/routes'));
+
+app.use('/', express.static(path.resolve(__dirname, 'dist')));
 
 app.get('*', (req, res) => {
   return res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
